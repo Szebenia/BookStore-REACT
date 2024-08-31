45166bd66ad40553c243558b102da4efb58a4d97
Szebenia <atis.8021@gmail.com>
2024-08-31 14:53:46 +0200

    update1

--- BookCard.jsx
+++ BookCard.jsx
@@ -1,12 +1,14 @@
 import { useContext, useState } from "react";
 import BookStoreContext from "./BookStoreContext.js";
 
-function BookCard({ book, currentCurrency }) {
+function BookCard({ book, currentCurrency, inputCheckbox }) {
   const { currencies } = useContext(BookStoreContext);
   const [read, setRead] = useState(false);
 
   return (
     <div className="bookcard">
+      {inputCheckbox === read || inputCheckbox === false ? 
+      <>
       <h2>{book.title}</h2>
       <p>
         {Math.round(
@@ -14,7 +16,7 @@ function BookCard({ book, currentCurrency }) {
         ) / 100}{" "}
         {currencies[currentCurrency].symbol}
       </p>
-      <button onClick={() => setRead(!read)}>{read ? "Elolvastam" : "Még nem olvastam"}</button>
+      <button onClick={() => setRead(!read)}>{read ? "Elolvastam" : "Még nem olvastam"}</button></> : null}
     </div>
   );
 }
--- BookStore.jsx
+++ BookStore.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import BookStoreProvider from "./BookStoreProvider.jsx";
 import BookCard from "./BookCard.jsx";
 
@@ -14,19 +14,47 @@ function BookStore() {
       title: "The Road to GraphQL",
       price: 29.99,
     },
+    {
+      id: 3,
+      title: "A Road to React",
+      price: 19.99,
+    },
+    {
+      id: 4,
+      title: "2 Roads to GraphQL",
+      price: 29.99,
+    },
+    {
+      id: 5,
+      title: "2 Different Roads to GraphQL",
+      price: 29.99,
+    },
   ];
+  const inputRef = useRef(null);
+  const inputCheckboxRef = useRef(null);
+
+  const [inputText, setInputText] = useState("");
+  const [inputCheckbox, setCheckbox] = useState(false);
 
   const [currency, setCurrency] = useState("euro");
+
+ 
 
   return (
     <BookStoreProvider>
       <div>
+        <div>
+        <input ref={inputRef} onChange={() => setInputText(inputRef.current.value)} type="text" placeholder="A keresett könyv címe"/>
+        </div>
+        <div>
+        <input ref={inputCheckboxRef} onChange={() => {setCheckbox(inputCheckboxRef.current.checked); console.log(inputCheckboxRef.current.checked)}} type="checkbox" />
+        </div>
         <button onClick={() => setCurrency("euro")}>EUR</button>
         <button onClick={() => setCurrency("usd")}>USD</button>
         <button onClick={() => setCurrency("forint")}>HUF</button>
 
-        {books.map((book) => (
-          <BookCard key={book.id} book={book} currentCurrency={currency} />
+        {books.filter((book) => book.title.includes(inputText)).map((book) => (
+          <BookCard key={book.id} book={book} currentCurrency={currency} inputCheckbox={inputCheckbox}/>
         ))}
       </div>
     </BookStoreProvider>
